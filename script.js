5b30288db488138478e52284248c185333bb665e
DalaScript <nik.dalalishvili@gmail.com>
2025-06-03 23:07:51 +0400

    last fix

--- script.js
+++ script.js
@@ -16,17 +16,12 @@ function showModal() {
 
 // Modal Event Listeners
 modalShow.addEventListener("click", showModal);
-modalClose.addEventListener("click", () =>
-    modal.classList.remove("show-modal")
-);
-window.addEventListener("click", (e) =>
-    e.target === modal ? modal.classList.remove("show-modal") : false
-);
+modalClose.addEventListener("click", () => modal.classList.remove("show-modal"));
+window.addEventListener("click", (e) => e.target === modal ? modal.classList.remove("show-modal") : false );
 
 // Validate Form
 function validate(nameValue, urlValue) {
-    const expression =
-        /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+    const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
     const regex = new RegExp(expression);
     if (!nameValue || !urlValue) {
         alert("Please submit values for both fields.");
@@ -61,10 +56,7 @@ function buildBookmarks() {
         linkInfo.classList.add("name");
         // Favicon
         const favicon = document.createElement("img");
-        favicon.setAttribute(
-            "src",
-            `https://s2.googleusercontent.com/s2/favicons?domain=${url}`
-        );
+        favicon.setAttribute("src", `https://s2.googleusercontent.com/s2/favicons?domain=${url}`);
         // Link
         const link = document.createElement("a");
         link.setAttribute("href", `${url}`);
