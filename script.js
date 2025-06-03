44da2e1c8e944c7371e70510e28041978f0758ef
DalaScript <nik.dalalishvili@gmail.com>
2025-06-03 22:44:27 +0400

    build bookmarks

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const modalClose = document.getElementById("close-modal");
 const bookmarkForm = document.getElementById("bookmark-form");
 const websiteNameEl = document.getElementById("website-name");
 const websiteUrlEl = document.getElementById("website-url");
-const bookmarkContainer = document.getElementById("bookmarks-container");
+const bookmarksContainer = document.getElementById("bookmarks-container");
 
 let bookmarks = [];
 
@@ -40,6 +40,38 @@ function validate(nameValue, urlValue) {
     return true;
 }
 
+// Build Bookmarks DOM
+function buildBookmarks() {
+    // Build items
+    bookmarks.forEach((bookmark) => {
+        const { name, url } = bookmark;
+        console.log(name, url);
+        // Item
+        const item = document.createElement('div');
+        item.classList.add('item');
+        // Close Icon
+        const closeIcon = document.createElement('i');
+        closeIcon.classList.add('fas', 'fa-times');
+        closeIcon.setAttribute('title', 'Delete Bookmark');
+        closeIcon.setAttribute('onclick', `deleteBookmark('${url}')`);
+        // Favicon / Link Container
+        const linkInfo = document.createElement('div');
+        linkInfo.classList.add('name');
+        // Favicon
+        const favicon = document.createElement('img');
+        favicon.setAttribute('src', `https://s2.googleusercontent.com/s2/favicons?domain=${url}`);
+        // Link
+        const link = document.createElement('a');
+        link.setAttribute('href', `${url}`);
+        link.setAttribute('target', '_blank');
+        link.textContent = name;
+        // Append to bookmarks container
+        linkInfo.append(favicon, link);
+        item.append(closeIcon, linkInfo);
+        bookmarksContainer.appendChild(item);
+    });
+}
+
 // Fetch Bookmarks
 function fetchBookmarks() {
     // Get bookmarks from localStorage if available
@@ -53,9 +85,9 @@ function fetchBookmarks() {
                 url: "https://github.com/DalaScript",
             },
         ];
-        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+        localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
     }
-    console.log(bookmarks);
+    buildBookmarks();
 }
 
 // Handle Data from Form
